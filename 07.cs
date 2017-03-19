7267956285b258175973e581b99d4cc6ae8c2c12
Zane <zanesuhi@gmail.com>
2017-03-19 11:18:36 +0100

    Velocity calculating.

---
+++ 07.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double velocity;
+            double time;
+            double gravity = 9.81;
+
+            Console.WriteLine("Write the desired time in seconds: ");
+            time = int.Parse(Console.ReadLine());
+
+            velocity = gravity * time;
+
+            Console.WriteLine("After {0} seconds the falling speed will be {1} m/s", time, velocity);
+            Console.ReadKey();
+        }
+    }
+}
