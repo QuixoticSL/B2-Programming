72ba6b7f68116959165183a781c5133e2d138ba9
Zane <zanesuhi@gmail.com>
2017-03-27 19:26:36 +0200

    Program that lists your name, surname, mayor and student ID

---
+++ 11.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            string surname = "Smith";
+            string mayor = "Information Science";
+
+            long studentNumber = 19127572;
+
+            Console.WriteLine("Your name is {0} {1}\nYour mayor is {2}\nand your student number is {3}", name, surname, mayor, studentNumber);
+            Console.ReadKey();
+        }
+    }
+}
