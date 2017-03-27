744317fbb03f65fa89584aecb09d5509c3d51099
Zane <zanesuhi@gmail.com>
2017-03-27 16:50:39 +0200

    Calculate number of digits without using to.String

---
+++ 09.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 12345;
+
+            int length = 0;
+
+            while (number > 0)
+            {
+                length++;
+                number /= 10;
+            }
+            if (length < 1) length = 1;
+
+            Console.WriteLine(length); // 5
+            Console.ReadKey();
+        }
+    }
+}
