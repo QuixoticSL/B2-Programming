9f5c4b7f75744c43f980322ee5f90aaf64f24466
Zane <zanesuhi@gmail.com>
2017-03-27 19:19:55 +0200

    Displays name, surname, street name, zip code...

---
+++ 10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "John";
+            string surname = "Smith";
+            string streetName = "Downing";
+            string place = "England";
+            int zipCode = 1200;
+            int streetNum = 17;
+
+            Console.WriteLine("{0}, {1}", name, surname);
+            Console.WriteLine("{0}, {1}", streetName, place);
+            Console.WriteLine("{0}, {1}", streetNum, zipCode);
+
+            Console.ReadKey();
+
+        }
+    }
+}
