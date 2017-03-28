543a1e99bcc797df41a9a759f47c842b389ba950
Zane <zanesuhi@gmail.com>
2017-03-28 20:21:37 +0200

    Shows lowe and upper case of inputed character.

---
+++ 12.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char character;
+            Console.WriteLine("Input character: ");
+            character = (char)Console.Read();
+
+            if (character >= 'a' && character <= 'z')
+            {
+                Console.WriteLine("Character {0} is lower case, uppercase should look like this: {1}", character, (char)(character - 32));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
