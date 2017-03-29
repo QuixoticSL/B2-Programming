2c3d21a87f606f58d60fd2ce49c6b9f15f9b516c
Zane <zanesuhi@gmail.com>
2017-03-29 17:53:19 +0200

    Program that plays like the jackpot game.

---
+++ 15.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pick a random number between 1 and 6
+            int num1, num2, num3;
+
+            Random r = new Random(); // picks from the random selection of numbers
+
+            num1 = r.Next(1, 7); // type in the range, but 6 isn't included so we type 7
+            num2 = r.Next(1, 7);
+            num3 = r.Next(1, 7);
+
+            if (num1 == num2 && num3 == num1)
+            {
+                Console.WriteLine("Jackpot!!!");
+            }
+
+            Console.WriteLine("{0}, {1}, {2}", num1, num2, num3);
+            Console.ReadKey();
+
+        }
+    }
+}
