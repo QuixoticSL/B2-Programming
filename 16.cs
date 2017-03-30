12a866cee1b2436c5eb00bd77b6c5a7af197fcf6
Zane <zanesuhi@gmail.com>
2017-03-30 17:39:10 +0200

    Program that calculates leap years, based on what year you input

---
+++ 16.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program that calculates leap years, based on what year you input
+            int year;
+            bool leap = false;
+
+            Console.WriteLine("Please enter year: ");
+            year = int.Parse(Console.ReadLine());
+
+            if (year % 4 == 0)
+            {
+                leap = true;
+            }
+            if (year % 100 == 0)
+            {
+                leap = false;
+            }
+            if (year % 400 == 0)
+            {
+                leap = true;
+            }
+
+            if (leap)
+            {
+                Console.WriteLine("This {0} is a leap year!", year);
+            }
+            else
+            {
+                Console.WriteLine("Fuck you this {0} isn't a leap year!", year);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
